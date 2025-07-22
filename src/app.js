166f0559afd6fc53552190040e243976b459fe1d
Your NamePrashant <prsharma0403@gmail.com>
2025-07-22 19:49:08 +0530

    changes for route and try catch

--- src/app.js
+++ src/app.js
@@ -1,44 +1,38 @@
 console.log("starting the project")
 
-const express = require('express')
+const express = require('express');
+const { adminauth,userauth } = require('../middlewares/auth');
 const app = express()
-const port = 8955
+const port = 8955;
 
-/*app.get('/user/:userId/:username/:password', (req, res) => {
-    console.log(req.params)
-  res.send({firstname:"prashant",lastname:"Praveen"})
+app.get("/getUserData",(req,res)=>{
+  try{
+    throw new Error("djfhjsdhfh")
+    res.send("User Data Sent")
+  }
+  catch (err){
+    res.status(500).send("Contact support ");
+  }
+});
+/*app.use("/admin",adminauth)
+app.get("/admin/getAllData",(req,res)=>{
+    
+        res.send("All Data Sent")
 })
-app.post('/user', (req, res) => {
-    console.log("Save data to the DataBase")
-    res.send("Data Saved Successfully")
-  })
-
-  app.delete('/user', (req, res) => {
-    console.log("Delete data to the DataBase")
-    res.send("Data delete Successfully")
-  })
-*/
-/*app.use("/test/test1",(req,res)=>{
-    res.send('Hello  From server !')
+app.get("/user",userauth,(req,res)=>{
+    res.send("UserData is Created")
 })
-app.use("/test",(req,res)=>{
-    res.send('Hello  From server test!')
+app.post("/user/login",(req,res)=>{
+  res.send("user logged successfully")
 })
-app.use("/dashboard",(req,res)=>{
-    res.send('Hello  From dashboard !')
-})
-
-app.use("/hello",(req,res)=>{
-    res.send('Hello  Hello  hello!')
+app.get("/admin/deleteUserData",(req,res)=>{
+    res.send("UserDeleted");
 })*/
-app.use("/user",(req,res,next)=>{
-   
-    next();
-    res.send('Namaste 🙏 User')
-    console.log("user1")
-},(req,res)=>{
-    res.send("Namaste 🙏 User 2")
-    console.log("user2")
+
+app.use("/",(err,req,res,next)=>{
+  if(err){
+    res.status(500).send("Something went Wrorng");
+  }
 })
 app.listen(port, () => {
   console.log(`Server is Successfully  listening on port ${port}`)
